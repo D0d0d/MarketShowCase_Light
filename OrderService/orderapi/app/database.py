5db23d8d34d68244e159a50c268806ca091df7b2
D0d0d <dodo@dodothere.space>
2023-12-15 15:55:00 +0300

    2

--- OrderService/orderapi/app/database.py
+++
@@ -1,8 +0,0 @@
-from pymongo import mongo_client, ASCENDING
-from app.config import settings
-
-client = mongo_client.MongoClient(settings.DATABASE_URL)
-
-db = client[settings.MONGO_INITDB_DATABASE]
-Orders = db.orders
-ProductItems = db.productItems
